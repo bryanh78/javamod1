fda2b6726e1d69e434ae98dd98984de65dce1688
Student <student@RU-008.local>
2016-01-18 15:24:51 -0700

    mod3done

--- mod1.js
+++ mod1.js
@@ -15,6 +15,49 @@
 // 	console.log(i)
 // }
 
-var word= prompt('enter any word')
-alert(word+ "\n" +word.length+"\n"+word.charAt(2)+"\n"+word.toLowerCase()+"\n"+word.toUpperCase()+"\n"+"Is "+word+"the word you wanted to use?"+"\n"+word.substring(1,4))
+// var word= prompt('enter any word')
+// alert(word+ "\n" +word.length+"\n"+word.charAt(2)+"\n"+word.toLowerCase()+"\n"+word.toUpperCase()+"\n"+"Is "+word+"the word you wanted to use?"+"\n"+word.substring(1,4))
+
+
+var phone = prompt("Please enter your phone number.", "(xxx-xxx-xxxx)")
+if (phone.charAt(3,7) === "-") {
+	var dob = prompt('Please enter your date of birth.', '(xx/xx/xx)')
+		if(dob.charAt(2,5) === "/") {
+			var zip = prompt('now your zipcode.','xxxxx or xxxxx-xxxx')
+				if(zip.length === 5 || zip.length === 10){
+					var state = prompt('State?','(ex:CO)')
+						if(state.length === 2 && state.toUpperCase()) {
+							var married = prompt('Are you married?','(yes/no)') 
+								if(married.toLowerCase() === 'yes'|| married.toLowerCase() === 'no') {
+									alert("Thank you... sucker")
+								}
+								else {
+									alert("invalid answer")
+								};
+						}
+						else {
+							alert("invalid answer")
+						};
+
+				}
+				else {
+					alert("invalid answer")
+				}
+		}
+		else {
+			alert("invalid answer")
+		};
+}
+else {
+	alert("invalid answer")
+};
+
+
+
+
+
+
+
+
+
 
