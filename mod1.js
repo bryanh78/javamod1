51a1eb2711b788bf59adc38b771a381fc1902c0b
Student <student@RU-008.local>
2016-01-18 12:10:29 -0700

    mod2

---
+++ mod1.js
@@ -0,0 +1,20 @@
+
+
+
+
+// var name = prompt('What is your name?');
+// alert('Hello ' + name);
+
+// for(var i = 0 ; i < 600; i +=100) {
+// 	console.log (i);
+// }
+// for(var i = 0; i < 4;  i++) {
+// 	console.log (i,i,i)
+// }
+// for(var i = 1; i <= 100; i*=2) {
+// 	console.log(i)
+// }
+
+var word= prompt('enter any word')
+alert(word)
+console.log(word.length)
