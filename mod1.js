6fe3e1228c8e70c12947ca2c2d2538888a67b9c4
Student <student@RU-008.local>
2016-01-18 17:30:06 -0700

    stuff

--- mod1.js
+++ mod1.js
@@ -19,42 +19,72 @@
 // alert(word+ "\n" +word.length+"\n"+word.charAt(2)+"\n"+word.toLowerCase()+"\n"+word.toUpperCase()+"\n"+"Is "+word+"the word you wanted to use?"+"\n"+word.substring(1,4))
 
 
-var phone = prompt("Please enter your phone number.", "(xxx-xxx-xxxx)")
-if (phone.charAt(3,7) === "-") {
-	var dob = prompt('Please enter your date of birth.', '(xx/xx/xx)')
-		if(dob.charAt(2,5) === "/") {
-			var zip = prompt('now your zipcode.','xxxxx or xxxxx-xxxx')
-				if(zip.length === 5 || zip.length === 10){
-					var state = prompt('State?','(ex:CO)')
-						if(state.length === 2 && state.toUpperCase()) {
-							var married = prompt('Are you married?','(yes/no)') 
-								if(married.toLowerCase() === 'yes'|| married.toLowerCase() === 'no') {
-									alert("Thank you... sucker")
-								}
-								else {
-									alert("invalid answer")
-								};
-						}
-						else {
-							alert("invalid answer")
-						};
+// var phone = prompt("Please enter your phone number.", "(xxx-xxx-xxxx)")
+// if (phone.charAt(3,7) === "-") {
+// 	var dob = prompt('Please enter your date of birth.', '(xx/xx/xx)')
+// 		if(dob.charAt(2,5) === "/") {
+// 			var zip = prompt('now your zipcode.','xxxxx or xxxxx-xxxx')
+// 				if(zip.length === 5 || zip.length === 10){
+// 					var state = prompt('State?','(ex:CO)')
+// 						if(state.length === 2 && state.toUpperCase()) {
+// 							var married = prompt('Are you married?','(yes/no)') 
+// 								if(married.toLowerCase() === 'yes'|| married.toLowerCase() === 'no') {
+// 									alert("Thank you... sucker")
+// 								}
+// 								else {
+// 									alert("invalid answer")
+// 								};
+// 						}
+// 						else {
+// 							alert("invalid answer")
+// 						};
 
-				}
-				else {
-					alert("invalid answer")
-				}
-		}
-		else {
-			alert("invalid answer")
-		};
+// 				}
+// 				else {
+// 					alert("invalid answer")
+// 				}
+// 		}
+// 		else {
+// 			alert("invalid answer")
+// 		};
+// }
+// else {
+// 	alert("invalid answer")
+// };
+
+var victims = prompt("How many disaster victims are there?")
+
+victimsInfo=[]
+
+for(i=0; i<victims.length;i++) {
+	var vicName = prompt("Victims Name")
+	victimsinfo.push("'"+vicName+"'")
+
+	var vicNum = prompt("Victims Number")
+	victimsinfo.push(vicNum)
+
+	var vicAdd = prompt("Victims Address")
+	victimsinfo.push(vicAdd)
 }
-else {
-	alert("invalid answer")
-};
 
+var volunteers = prompt("How many volunteers can sign up to help?")
 
+volunteersInfo = []
 
+for(i=0; i<volunteers;i++) {
+	var volName = prompt("Volunteers Name")
+	volunteersInfo.push(volName)
+}
 
+for(i=0; i<volunteers;i++) {
+	var volNum = prompt("Volunteers Number")
+	volunteersInfo.push(volNum)
+}
+
+for(i=0; i<volunteers;i++) {
+	var volAdd = prompt("Volunteers Address")
+	volunteersInfo.push(volAdd)
+}
 
 
 
